245f3c6d2d943b6a0825d924fca1aec9561d00db
vkrishnajayanthi <vkrishnajayanthi>
2017-10-28 12:52:08 -0400

    04: Cleanup of code

--- 04/LightsAndTextures/src/View.java
+++ 04/LightsAndTextures/src/View.java
@@ -35,6 +35,8 @@ public class View {
   private int WINDOW_WIDTH, WINDOW_HEIGHT;
   private Matrix4f proj;
   private Stack<Matrix4f> modelView;
+
+  /*
   private List<ObjectInstance> meshObjects;
   private List<util.TextureImage> textures;
   private List<util.Material> materials;
@@ -42,6 +44,7 @@ public class View {
   private List<util.Light> lights;
   //0-meshObjects.size()-1 are object coordinates, then world and then view
   private List<Integer> lightCoordinateSystems;
+  */
   private Matrix4f trackballTransform, textureTransform;
   private float trackballRadius;
   private Vector2f mousePos;
@@ -69,6 +72,7 @@ public class View {
 
     modelView = new Stack<Matrix4f>();
 
+    /*
     meshObjects = new ArrayList<ObjectInstance>();
     transforms = new ArrayList<Matrix4f>();
     materials = new ArrayList<util.Material>();
@@ -77,6 +81,7 @@ public class View {
     lightLocations = new ArrayList<LightLocation>();
     textures = new ArrayList<util.TextureImage>();
     textureTransform = new Matrix4f();
+    */
 
     trackballTransform = new Matrix4f();
     angleOfRotation = 0;
@@ -89,106 +94,12 @@ public class View {
     mipmapped = !mipmapped;
   }
 
-  private void initObjects(GL3 gl) throws FileNotFoundException, IOException {
-
-    util.PolygonMesh<?> tmesh;
-
-    InputStream in;
-
-    in = getClass().getClassLoader().getResourceAsStream
-            ("models/sphere.obj");
-
-    tmesh = util.ObjImporter.importFile(new VertexAttribProducer(), in, true);
-    util.ObjectInstance obj;
-
-    Map<String, String> shaderToVertexAttribute = new HashMap<String, String>();
-
-    shaderToVertexAttribute.put("vPosition", "position");
-    shaderToVertexAttribute.put("vNormal", "normal");
-    shaderToVertexAttribute.put("vTexCoord", "texcoord");
-
-
-    obj = new util.ObjectInstance(
-            gl,
-            program,
-            shaderLocations,
-            shaderToVertexAttribute,
-            tmesh, new String(""));
-    meshObjects.add(obj);
-    util.Material mat;
-
-    mat = new util.Material();
-
-    mat.setAmbient(0.3f, 0.3f, 0.3f);
-    mat.setDiffuse(0.7f, 0.7f, 0.7f);
-    mat.setSpecular(0.7f, 0.7f, 0.7f);
-    mat.setShininess(100);
-    materials.add(mat);
-
-    Matrix4f t;
-
-    t = new Matrix4f().translate(-200, 0, 0).scale(1, 1, 1);
-    transforms.add(t);
-
-    // textures
-
-    util.TextureImage textureImage;
-
-    textureImage = new util.TextureImage("textures/white.png",
-            "png",
-            "earthmap");
-
-    Texture tex = textureImage.getTexture();
-
-
-    tex.setTexParameteri(gl, GL.GL_TEXTURE_WRAP_S, GL.GL_REPEAT);
-    tex.setTexParameteri(gl, GL.GL_TEXTURE_WRAP_T, GL.GL_REPEAT);
-    tex.setTexParameteri(gl, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR);
-    tex.setTexParameteri(gl, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR);
-
-    textures.add(textureImage);
-
-
-  }
-
-  private void initLights() {
-    util.Light l = new util.Light();
-    l.setAmbient(0.8f, 0.8f, 0.8f);
-    l.setDiffuse(0.5f, 0.5f, 0.5f);
-    l.setSpecular(0.5f, 0.5f, 0.5f);
-    l.setPosition(00, 00, 100);
-    //lights.add(l);
-    //read comments above where this list is declared
-    //lightCoordinateSystems.add(meshObjects.size()); //world
-
-
-  }
-
   private void initShaderVariables() {
     //get input variables that need to be given to the shader program
     projectionLocation = shaderLocations.getLocation("projection");
     modelviewLocation = shaderLocations.getLocation("modelview");
-    normalmatrixLocation = shaderLocations.getLocation("normalmatrix");
-    texturematrixLocation = shaderLocations.getLocation("texturematrix");
-    materialAmbientLocation = shaderLocations.getLocation("material.ambient");
-    materialDiffuseLocation = shaderLocations.getLocation("material.diffuse");
-    materialSpecularLocation = shaderLocations.getLocation("material.specular");
-    materialShininessLocation = shaderLocations.getLocation("material.shininess");
-
-    textureLocation = shaderLocations.getLocation("image");
-
     numLightsLocation = shaderLocations.getLocation("numLights");
-    for (int i = 0; i < lights.size(); i++) {
-      LightLocation ll = new LightLocation();
-      String name;
 
-      name = "light[" + i + "]";
-      ll.ambient = shaderLocations.getLocation(name + "" + ".ambient");
-      ll.diffuse = shaderLocations.getLocation(name + ".diffuse");
-      ll.specular = shaderLocations.getLocation(name + ".specular");
-      ll.position = shaderLocations.getLocation(name + ".position");
-      lightLocations.add(ll);
-    }
   }
 
   public void initSceneGraph(GLAutoDrawable gla) throws Exception {
@@ -199,7 +110,7 @@ public class View {
     InputStream in = getClass().getClassLoader().getResourceAsStream(SCENE_GRAPH_XML);
 
 
-    program.enable(gl);
+    //program.enable(gl);
 
     scenegraph = sgraph.SceneXMLReader.importScenegraph(in, new VertexAttribProducer());
     renderer = new sgraph.GL3ScenegraphRenderer();
@@ -211,7 +122,7 @@ public class View {
     renderer.initShaderProgram(program, shaderVarsToVertexAttribs);
     scenegraph.setRenderer(renderer);
 
-    program.disable(gl);
+    //program.disable(gl);
   }
 
   public void init(GLAutoDrawable gla) throws Exception {
@@ -224,8 +135,6 @@ public class View {
             "shaders/phong-multiple.frag");
     shaderLocations = program.getAllShaderVariables(gl);
 
-    initObjects(gl);
-    initLights();
     initShaderVariables();
     initSceneGraph(gla);
   }
@@ -239,7 +148,6 @@ public class View {
 
     GL3 gl = gla.getGL().getGL3();
     FloatBuffer fb16 = Buffers.newDirectFloatBuffer(16);
-    FloatBuffer fb4 = Buffers.newDirectFloatBuffer(4);
 
     gl.glClearColor(0, 0, 0, 1);
     gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT);
@@ -256,7 +164,6 @@ public class View {
      *Supply the shader with all the matrices it expects.
     */
     gl.glUniformMatrix4fv(projectionLocation, 1, false, proj.get(fb16));
-    //return;
 
     if(scenegraph != null) {
       while (!modelView.empty())
@@ -269,23 +176,13 @@ public class View {
       //all the light properties, except positions
       gl.glUniform1i(numLightsLocation, renderer.getLightCount());
 
-      //System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
       renderer.drawLight();
       renderer.drawMeshes();
-
-      //System.out.println("-------------------------------------------------------------------------");
-
-    } else {
-      //all the light properties, except positions
-      gl.glUniform1i(numLightsLocation, lights.size());
-
     }
 
     gl.glFlush();
 
     program.disable(gl);
-
-
   }
 
   public void mousePressed(int x, int y) {
